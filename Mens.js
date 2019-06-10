58fc22f1ae10caf805894e55fa0e7297497b2db2
Stephen Fowles-White <FowlesWhite@gmail.com>
2019-06-10 22:22:45 +0100

    uploaded content pages

---
+++ Mens.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Mens extends Component {
+  render() {
+    return (
+      <div>
+        <h1> This is Mens</h1>
+      </div>
+    )
+  }
+}
---
+++ kids.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Kids extends Component {
+  render() {
+    return (
+      <div>
+      <h1>Kids</h1>
+        
+      </div>
+    )
+  }
+}
