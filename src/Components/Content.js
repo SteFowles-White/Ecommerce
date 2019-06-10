2a7e23ccd8fcfff11234ab9966bbfbb292f6b17b
Stephen Fowles-White <FowlesWhite@gmail.com>
2019-06-10 22:25:40 +0100

    updated to get nave bar working

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
+import Home from './Home';
+import Mens from './Mens';
+import Women from './Womens';
+import Kids from './kids';
 
 export default class Content extends Component {
+  
+//This renders the Content based on the navigation selected
+Content = (props) => {
+  if(props.page === "Home") {return <Home />}
+  else if (props.page === "Mens"){return <Mens />}
+  else if (props.page === "Womens"){return <Women />}
+  else if (props.page === "Kids"){return <Kids />}
+}
+
+
   render() {
     return (
       <div>
-      <h1>Hello World</h1>
-        
+        {
+          this.Content(this.props)
+        }
+
       </div>
+
     )
   }
 }
